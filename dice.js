a770483c621a47e3d30cc5a5b9387ca2f77276fc
Andres Carrasco <andrescm1998@outlook.com>
2022-09-04 22:35:04 +0100

    Add animation

--- dice.js
+++ dice.js
@@ -1,58 +1,80 @@
-let yourScore = 0;
-let opponentScore = 0;
+let player1 = 0;
+let player2 = 0;
 let results = ['first','second','third'];
 let previous = [0,0,0];
+let diceFace = 0;
+let endRoll = 0;
+let lastNum = 0;
+let myInterval;
+
+//console.log(animationNumbers);
 
 
 window.onload = function() {
     let runButton = document.getElementById("run-button");
     runButton.src = "./images/run-button.png"
-    runButton.addEventListener("click", diceRun)
+    runButton.addEventListener("click", startAnimation);
 
 
 }
 
-function diceRun() {
+function startAnimation() {
+    myInterval = setInterval(updateImage, 100);
+}
 
-    let num = Math.ceil(Math.random()*6)
-    updateScore(num);
+function updateImage(){
 
-    document.getElementById("dice").src = "./images/" + num.toString() + ".png"
+    //console.log('Previous number: ' + previous[0]);
+    diceFace = Math.ceil(Math.random()*6);
+    if (diceFace == lastNum && diceFace == 6){
+        diceFace--;
+    }
+    else if (diceFace == lastNum) {
+        diceFace++;
+    }
+
+    lastNum = diceFace;
+    console.log(diceFace);
+    
+    document.getElementById("dice").src = "./images/" + diceFace.toString() + ".png";
+    endRoll++;
+    
+    if(endRoll == 10){
+        console.log('End animation')
+        endRoll = 0;
+        clearInterval(myInterval);
+        updateScore();
+    }
+}
+
+function updateScore() {
+
+    console.log('I am in update score')
+
+    playerOneGuess = document.getElementById("guess-1").value;
+    playerTwoGuess = document.getElementById("guess-2").value;
+
+    if (diceFace.toString() == playerOneGuess){
+        player1++;
+        console.log('Hey');
+    } else if (diceFace.toString() == playerTwoGuess) {
+        player2++;
+        console.log('Hey');
+    }
+
+    document.getElementById('player1').innerText = "Player 1 score: " + player1;
+
+    document.getElementById('player2').innerText = "Player 2 score: " + player2;
+
+
+    previous[2] = previous[1];
+    previous[1] = previous[0]
+    previous[0] = diceFace;
+
 
     for (let i = 0; i<3;i++) {
+        //Update previous results
         document.getElementById(results[i]).innerText = "-- " + previous[i].toString() + " --"
     }
 }
 
-function updateScore(num) {
-    previous[2] = previous[1];
-    previous[1] = previous[0]
-    previous[0] = num;
-}
-
-
-
-
-/*
-function displayResult (e) {
-    const diceNum = Math.floor(Math.random()*6) + 1;
-    
-    switch(diceNum){
-        case 1:
-        // show dice 1            
-        case 2:
-        // show dice 2
-        case 3:
-        // show dice 3
-        case 4:
-        // show dice 4
-        case 5:
-        // show dice 5
-        case 6:
-        // show dice 6
-    }
-
-}
-
-dice.addEventListener("click", displayResult);
-*/
